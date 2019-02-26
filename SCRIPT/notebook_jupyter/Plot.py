f3734ed49a2f76af87b26d39b0ebfa139fb14db4
pageotyann <pageoty@cesbio.cnes.fr>
2019-02-26 15:16:57 +0100

    new plot in script and notebook

--- SCRIPT/notebook_jupyter/Plot.py
+++ SCRIPT/notebook_jupyter/Plot.py
@@ -11,44 +11,100 @@ from matplotlib import cm
 import numpy as np 
 import seaborn as sns
 
-df=pd.read_csv("/datalocal/vboxshare/THESE/CLASSIFICATION/TRAITEMENT/STAT_POLY/STAT_2017_Pixel_cate_label_CrIrr.csv")
-name=["Maize_Nirr","Soybean_Nirr","Peas_Nirr","Sunflow_Nirr","Maize_Irr","Sorghum_Nirr","Sorghum_Irr","Soybean_Irr","Peas_Irr","Sunflow_Irr","Others"]
-y_pos = np.arange(len(name))
-df["name"]=name
 
+
+df=pd.read_csv("/datalocal/vboxshare/THESE/CLASSIFICATION/TRAITEMENT/STAT_POLY/STAT_2017_Pixel_cate_label_CrIrr.csv")
+
+# name=["Maize_Nirr","Soybean_Nirr","Peas_Nirr","Sunflow_Nirr","Maize_Irr","Sorghum_Nirr","Sorghum_Irr","Soybean_Irr","Peas_Irr","Sunflow_Irr","Others"]
+names_crop=["Maize_Irr","Soybean_Irr","Sorghum_Irr","Sunflower_Irr","Peas_Irr","Others","Maize_Nirr","Soybean_Nirr","Sorghum_Nirr","Sunflower_Nirr","Peas_Nirr"]
+#names_RPG=["Maize","Soybean","Sorghum","Sunflower","Peas"]
+
+df1=df.sort_values(by='category', ascending=True)
+df1["name"]=names_crop
+df1=df1.reset_index()
+y_pos = np.arange(len(list(df1.name)))
 df2=pd.read_csv("/datalocal/vboxshare/THESE/CLASSIFICATION/TRAITEMENT/STAT_POLY/STAT_2017_Pixel_cate_label_CrIrr_tcj.csv")
 df2
-name2=["Maize_Nirr","Soybean_Nirr","Peas_Nirr","Sunflow_Nirr","Maize_Irr","Sorghum_Nirr","Soybean_Irr","Sunflow_Irr","Others"]
-df2["name"]=name2
+name2=["Maize_Irr","Soybean_Irr","Sunflower_Irr","Others","Maize_Nirr","Soybean_Nirr","Sorghum_Nirr","Sunflow_Nirr","Peas_Nirr"]
 
-plt.figure(figsize=(10,5))
-sns.barplot(df.name,df["count"])
-plt.xticks(y_pos, name,rotation=45)
+df2=df2.sort_values(by='category', ascending=True)
+df2["name"]=name2
+df2=df2.reset_index()
+
+plt.figure(figsize=(15,15))
+plt.subplot(2,2,1)
+pal=sns.set_palette("RdYlBu",len(y_pos))
+sns.barplot(df1.name,df1["count"])
+plt.xticks(y_pos, names_crop,rotation=45)
 plt.xlabel("crops")
 plt.ylabel("Nombre de polygon")
-plt.title("Ensemble des 8 tiles")
+plt.title("Ensemble des 9 tiles")
 
-label=["n=691","n=153","n=20","n=469","n=2448","n=20","n=3","n=1247","n=4","n=6","n=193"]
+label=round(df1["count"],2)
 # Tet on the top of each barplot
-for i in range(len(df["count"])):
-    plt.text(x = y_pos[i] -0.3 , y = df["count"][i] +10, s = label[i], size = 11)
+for i in range(len(df1["count"])):
+    plt.text(x = y_pos[i] -0.3 , y = df1["count"][i] +10, s = label[i], size = 11)
 # Show graphic
-plt.show()
+#plt.show()
 
 
+plt.subplot(2,2,2)
 
-plt.figure(figsize=(10,5))
 y_pos2 = np.arange(len(name2))
 sns.barplot(df2.name,df2["count"])
-plt.xticks(y_pos2, name,rotation=45)
+plt.xticks(y_pos2, name2,rotation=45)
 plt.xlabel("crops")
 plt.ylabel("Nombre de polygon")
 plt.title("Tile TCJ")
 
-label=["n=227","n=86","n=13","n=318","n=1315","n=16","n=891","n=1","n=110"]
+label=round(df2["count"],2)
 # Tet on the top of each barplot
 for i in range(len(df2["count"])):
     plt.text(x = y_pos2[i] -0.3 , y = df2["count"][i] +10, s = label[i], size = 11)
 
 # Show graphic
+#plt.show()
+
+
+df=pd.read_csv("/datalocal/vboxshare/THESE/CLASSIFICATION/TRAITEMENT/STAT_POLY/STAT_DT_TCJ_LabCrirr.csv")
+df=df.sort_values(by='category', ascending=True)
+names_crop_tcj=["Maize_Irr","Soybean_Irr","Sunflower_Irr","Others","Maize_Nirr","Soybean_Nirr","Sorghum_Nirr","Sunflower_Nirr","Peas_Nirr"]
+df["name"]=names_crop_tcj
+df=df.reset_index()
+y_pos= np.arange(len(list(df.name)))
+plt.subplot(2,2,4)
+
+plt.xticks(y_pos, names_crop,rotation=45)
+plt.xlabel("OS")
+plt.ylabel("Réparition")
+plt.title("Tile TCJ")
+sns.barplot(df.name,df["sum"]/sum(df["sum"]),palette=pal)
+plt.text(x=df.shape[0]-2,y=max(df["sum"]/sum(df["sum"])),s=round(float(sum(df["sum"])),2))
+plt.text(x=df.shape[0]-4.5,y=max(df["sum"]/sum(df["sum"])),s="superfice totale (ha):")
+label=round(df["sum"]/sum(df["sum"]),2)
+        # Tet on the top of each barplot
+for j in range(len(df["sum"]/sum(df["sum"]))):
+    plt.text(x = y_pos[j] , y = (df["sum"]/sum(df["sum"]))[j], s = list(label)[j], size = 11)
+
+#plt.show()
+
+df=pd.read_csv("/datalocal/vboxshare/THESE/CLASSIFICATION/TRAITEMENT/STAT_POLY/STAT_DT_ALL_lab_Crirr.csv")
+df=df.sort_values(by='category', ascending=True)
+df["name"]=names_crop
+df=df.reset_index()
+y_pos= np.arange(len(list(df.name)))
+plt.subplot(2,2,3)
+
+plt.xticks(y_pos, names_crop,rotation=45)
+plt.xlabel("OS")
+plt.ylabel("Réparition")
+plt.title("9 tiles")
+sns.barplot(df.name,df["sum"]/sum(df["sum"]),palette=pal)
+plt.text(x=df.shape[0]-2,y=max(df["sum"]/sum(df["sum"])),s=round(float(sum(df["sum"])),2))
+plt.text(x=df.shape[0]-4.5,y=max(df["sum"]/sum(df["sum"])),s="superfice totale (ha):")
+label=round(df["sum"]/sum(df["sum"]),3)
+        # Tet on the top of each barplot
+for j in range(len(df["sum"]/sum(df["sum"]))):
+    plt.text(x = y_pos[j] -0.4 , y = (df["sum"]/sum(df["sum"]))[j], s = list(label)[j], size = 11)
+
 plt.show()
